65d85d0e0cccd32055f69e26ec67f93d694edd6f
Matthieu Rode <matthieu@eigo.ca>
2024-04-11 14:25:14 +0700

    Change place icon

--- src/assets/mapIcons.ts
+++ src/assets/mapIcons.ts
@@ -1,6 +1,6 @@
 export const geolocationIcon = {
-  path: google.maps.SymbolPath.CIRCLE,
-  scale: 10,
+  path: 'M10 0C4.477 0 0 4.477 0 10s4.477 10 10 10 10-4.477 10-10S15.523 0 10 0zm0 18.75c-4.663 0-8.437-3.774-8.437-8.438S5.337 2.875 10 2.875 18.437 6.649 18.437 11.312 14.663 18.75 10 18.75z',
+  scale: 1, // You can adjust the scale as needed
   fillColor: '#4285f4',
   fillOpacity: 1,
   strokeColor: 'white',
@@ -10,6 +10,6 @@ export const geolocationIcon = {
 export const defaultIcon = {};
 
 export const placeIcon = {
-  url: 'https://uploads-ssl.webflow.com/65a3fcd4290ce3c73b6bdce7/65e6015d85fe4634721617ed_pin-synapse.png', // Ensure url is a string, use empty string if null
+  url: 'https://uploads-ssl.webflow.com/65a3fcd4290ce3c73b6bdce7/6617902d8d3533cf0c3c4da8_pin-synapse.png', // Ensure url is a string, use empty string if null
   scaledSize: new google.maps.Size(40, 40),
 };
